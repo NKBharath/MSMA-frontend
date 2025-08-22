81ec302cb555dd39dbbaf1d906d39884ba1e702e
NKbharath <bharathnk2004@gmail.com>
2025-08-22 10:13:22 +0530

    changin backend to onrender

--- common/axiosInstance.js
+++ common/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
     baseURL: import.meta.env.MODE === "development"
         ? "http://localhost:3000/api"
-        : "https://msma-backend-production.up.railway.app/api",
+        : "https://msma-backend.onrender.com/api",
 });
 
 API.interceptors.request.use((req)=>{
